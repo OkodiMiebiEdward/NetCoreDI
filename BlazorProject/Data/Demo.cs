ebd2018deccac4420571fee0f07899e6aae4ec8a
Miebi <User@DESKTOP-1EK6ASL>
2024-05-17 14:06:25 +0100

    Chore:created demo class

---
+++ BlazorProject/Data/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorProject.Data
+{
+    public class Demo
+    {
+        public DateTime StartupTime { get; init; }
+        public Demo()
+        {
+            StartupTime = DateTime.Now;
+        }
+    }
+}
